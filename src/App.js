efcc9be0bfd78f16819c6f3d7083e43b398d91a0
Ana Mafla <anamafla@hotmail.com>
2018-11-08 20:54:49 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Example from "./Example";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Learning Hooks </h1>
+        <Example />
+      </div>
+    );
+  }
+}
+
+export default App;
